9902e8884f9da67aa135df5d42006d2d807be6be
Moh'd Al-Sulami <mmahmmad3878@gmail.com>
2023-10-24 08:39:49 +0300

    second changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +19,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/car', function () {
-    return view('home');
+Route::get('/db', function () {
+    // $users = DB::select('SELECT * FROM `users`');
+    // $users = DB::table('users')->get();
+    $users = User::get();
+    dd($users);
 });
-Route::view('/bag', 'home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
